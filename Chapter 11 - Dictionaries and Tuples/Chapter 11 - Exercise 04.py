80046296546871509b844872fb8b089ce6f941ea
Henry Rylko <henryrylko@gmail.com>
2017-10-12 14:38:47 -0500

    Finished up Chapter 11 Exercises and Studios

--- Chapter 11 - Dictionaries and Tuples/Chapter 11 - Exercise 04.py
+++ Chapter 11 - Dictionaries and Tuples/Chapter 11 - Exercise 04.py
@@ -18,3 +18,27 @@
 #  strings representing the 24 hour clock (e.g., "08:00"), and then figure out
 #  how to calculate the time worked. And you can do this exercise either by
 #  aliasing or copying the dictionary.
+
+
+def clock_in(dict, worker_name, clock_in_time):
+    dict[worker_name] = [clock_in_time, 0, 0]
+
+
+def clock_out(dict, worker_name, clock_out_time):
+    dict[worker_name][1] = clock_out_time
+    dict[worker_name][2] = clock_out_time - dict[worker_name][0]
+
+
+def main():
+    workers = {
+        "George Spelvin": [0, 0, 0],
+        "Jane Doe": [0, 0, 0],
+        "John Smith": [0, 0, 0]}
+    print(workers.get("George Spelvin"))   # should print [0,0,0]
+    clock_in(workers, "George Spelvin", 8)
+    clock_out(workers, "George Spelvin", 17)
+    print(workers.get("George Spelvin"))   # should print [8, 17, 9]
+
+
+if __name__ == "__main__":
+    main()
---
+++ Chapter 11 - Dictionaries and Tuples/Chapter 11 - Studio 11 - Bonus Mission - Reverse.py
@@ -0,0 +1,12 @@
+#  Create a function reverse that takes in a dictionary and reverses it, such
+#  that all of the values become keys and all of the keys become values. Be
+#  careful: we do not wish to lose any information. Consider what to do if the
+#  original dictionary has lists of values for a particular key, or has
+#  duplicate values for some keys.
+
+#  I chose not to complete this Bonus Mission because I thought that the
+#  requirements were vague (at best).  In general, I don't believe that
+#  dictionaries are reversible, since keys must be immutable, but values can
+#  have any type.  While it may be possible to handle the case of values which
+#  are lists of immutable values, I think it would be too kludgy to try to
+#  constrain the dictionary to these special cases.
