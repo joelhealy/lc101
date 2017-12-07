c7044a4492a8d30d56b56321573577605a74b71e
Joel Healy <joel.h.healy@gmail.com>
2017-12-07 11:06:13 -0600

    Began adding User model

--- unit2/get-it-done/main.py
+++ unit2/get-it-done/main.py
@@ -18,6 +18,16 @@ class Task(db.Model):
         self.completed = False
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+
 @app.route('/login')
 def login():
     return render_template('login.html')
